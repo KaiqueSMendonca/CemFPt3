09f41f0b0df1f4554ff4540900bd6ad987b690ef
KaiqueSMendonca <kaique.mendonca@etec.sp.gov.br>
2020-06-13 14:20:03 -0300

    C em F parte 3

---
+++ src/cemfpt3/CemFPt3.java
@@ -0,0 +1,14 @@
+package cemfpt3;
+
+public class CemFPt3 {
+
+    public static void main(String[] args) {
+        int i = 10;
+        for (i=10; i<=100; i+= 10){
+            double f = 9*i/5+32;
+            System.out.println(i+" = "+f);
+        }
+
+    }
+    
+}
